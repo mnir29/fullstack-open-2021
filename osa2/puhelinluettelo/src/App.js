5c38ac7afa62b7605fb966b0a95dc89e6e77486d
Markku <markku.nirkkonen@tuni.fi>
2021-04-29 21:45:07 +0300

    2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const updateNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNameSubmit}>
+        <div>
+          name: <input onChange={updateNewName} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>
+      })}
+    </div>
+  )
+
+}
+
+export default App
